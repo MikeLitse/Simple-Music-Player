7d9a942e930850336ff8614180ddc3dcee0316fb
Mike <mihalispro@gmail.com>
2026-09-17 21:53:35 +0300

    added exceptions

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
 
+import javax.sound.sampled.*;
 import java.util.*;
 void main() {
-    System.out.println("Hello world");
+
+    String filePath= "src\\This_Love.wav";
+    File file = new File(filePath);
+
+    try(AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(file)){
+
+        Clip clip = AudioSystem.getClip();
+        clip.open(audioInputStream);
+
+        System.out.println("Not Problem detected");
+
+    }
+    catch(UnsupportedAudioFileException e){
+        System.out.println("Unsupported Audio File");
+    }
+    catch(IOException e){
+        System.out.println("IO Exception");
+    }
+    catch(LineUnavailableException e){
+        System.out.println("Line Unavailable");
+    }
+    finally {
+        System.out.println("Program Terminated");
+    }
+
 }
