2765626d98cd64025860e780ff0f1d79a030cbf2
Mike <mihalispro@gmail.com>
2026-09-17 22:01:42 +0300

    added the loop

--- src/Main.java
+++ src/Main.java
@@ -6,10 +6,32 @@ void main() {
     String filePath= "src\\This_Love.wav";
     File file = new File(filePath);
 
-    try(AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(file)){
+    try(Scanner scanner = new Scanner(System.in);
+        AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(file)){
 
+        //Clip object to start the audio
         Clip clip = AudioSystem.getClip();
         clip.open(audioInputStream);
+
+        String response="";
+        //While to display what the user can input
+        while(!response.equals("Q")){
+            System.out.println("P = Play");
+            System.out.println("S = Stop");
+            System.out.println("R = Reset");
+            System.out.println("Q = Quit");
+
+            response=scanner.next().toUpperCase();
+
+            switch(response){
+                case "P" -> clip.start();
+                case "S" -> clip.stop();
+                case "R" -> clip.setMicrosecondPosition(0);
+            }
+
+        }
+        //switch to handle input
+
 
         System.out.println("Not Problem detected");
 
